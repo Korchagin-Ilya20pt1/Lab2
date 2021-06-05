84181894109c783d3c04c0a381939f36151d92ae
Korchagin-Ilya20pt1 <KorchaginIlya2002@yandex.ru>
2021-06-06 01:01:59 +0300

    Lab2

---
+++ 2/main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <iostream>
+#include <cctype>
+#include "PerestanovkaCipher.h"
+#include <locale>
+using namespace std;
+void check(const wstring& Text, const int & key)
+{
+    wstring s=Text;
+    try {
+        PerestanCipher skey(key);
+        wcout<<skey.CoderPerestanCipher(key, s)<<endl;
+        wstring g=skey.CoderPerestanCipher(key, s);
+        wcout<<skey.DecoderPerestanCipher(key,g)<<endl;
+    } catch (const cipher_error & e) {
+        cerr<<"Error: "<<e.what()<<endl;
+    }
+}
+int main()
+{
+    locale loc("ru_RU.UTF-8");
+    locale::global(loc);
+    check(L"KorchaginIlya",5);
+    check(L"KorchaginIlya",10);
+    check(L"Ilya",3);
+    return 0;
+}
